50a21087268ab1790e474556b04b26e2566d0bc8
sxinger <19035729+sxinger@users.noreply.github.com>
2023-04-22 03:51:16 +0000

    update send valueset

--- src/R/send_valueset.R
+++ src/R/send_valueset.R
@@ -24,58 +24,23 @@ pacman::p_load(
 
 source_url("https://raw.githubusercontent.com/sxinger/utils/master/extract_util.R")
 
-tgt_schema <- "OBESITY_OSA"
-tgt_tbl <- "OSA_CPAP_COV_VS"
+tgt_schema <- "PIM2016"
+tgt_tbl <- "PIM_VS_RXNORM"
 
 # make db connection
-sf_conn <- DBI::dbConnect(drv = odbc::odbc(),
-                          dsn = Sys.getenv("ODBC_DSN_NAME"),
-                          uid = Sys.getenv("SNOWFLAKE_USER"),
-                          pwd = Sys.getenv("SNOWFLAKE_PWD"))
+sf_conn <- DBI::dbConnect(
+  drv = odbc::odbc(),
+  dsn = Sys.getenv("ODBC_DSN_NAME"),
+  uid = Sys.getenv("SNOWFLAKE_USER"),
+  pwd = Sys.getenv("SNOWFLAKE_PWD")
+)
 
-##==== curated valuesets
-cov_vec<-c("coronary artery disease",
-           "bariatric surgery",
-           "chronic obstructive pulmonary disease",
-           "neurotic disorders",
-           "hypersomnia",
-           "insomina")
-
-for (i in seq_along(cov_vec)){
-  load_valueset(vs_template = "curated",
-                vs_url = "https://raw.githubusercontent.com/sxinger/PheCDM/main/valuesets/valueset_curated/vs-osa-comorb.json",
-                vs_name_str = cov_vec[i],
-                dry_run = TRUE,
-                conn=sf_conn,
-                write_to_schema = tgt_schema,
-                write_to_tbl = tgt_tbl,
-                overwrite = (i == 1))
-}
-
-##==== ecqm valuesets
-load_valueset(vs_template = "ecqm",
-              vs_url = "https://raw.githubusercontent.com/sxinger/PheCDM/main/valuesets/valueset_autogen/ecqm-condition-diagnosis-problem.json",
-              vs_name_str = "kidney failure",
-              dry_run = FALSE,
-              conn=sf_conn,
-              write_to_schema = tgt_schema,
-              write_to_tbl = tgt_tbl,
-              overwrite = FALSE)
-
-load_valueset(vs_template = "ecqm",
-              vs_url = "https://raw.githubusercontent.com/sxinger/PheCDM/main/valuesets/valueset_autogen/ecqm-medication.json",
-              vs_name_str = "antipsychotic",
-              dry_run = FALSE,
-              conn=sf_conn,
-              write_to_schema = tgt_schema,
-              write_to_tbl = tgt_tbl,
-              overwrite = FALSE)
-
-load_valueset(vs_template = "ecqm",
-              vs_url = "https://raw.githubusercontent.com/sxinger/PheCDM/main/valuesets/valueset_autogen/ecqm-medication.json",
-              vs_name_str = "Pharmacologic Therapy for Hypertension",
-              dry_run = FALSE,
-              conn=sf_conn,
-              write_to_schema = tgt_schema,
-              write_to_tbl = tgt_tbl,
-              overwrite = FALSE)
+load_valueset(
+  vs_template = "vsac",
+  vs_url = "https://raw.githubusercontent.com/RWD2E/phecdm/main/res/valueset_autogen/pim-rx.json",
+  dry_run = TRUE,
+  conn=sf_conn,
+  write_to_schema = tgt_schema,
+  write_to_tbl = tgt_tbl,
+  overwrite = TRUE
+)
