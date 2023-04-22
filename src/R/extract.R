0ba50d25ed17338bfd48478f51284d5d5db9fe90
sxinger <19035729+sxinger@users.noreply.github.com>
2023-04-22 09:41:00 +0000

    add data extraction script

--- src/R/extract.R
+++ src/R/extract.R
@@ -4,3 +4,27 @@
 # Description:
 # Dependency: 
 #################################################################
+
+rm(list=ls()); gc()
+setwd("C:/repos/pim_geriatric_cdm")
+
+# install.packages("pacman")
+pacman::p_load(
+  DBI,
+  jsonlite,
+  odbc,
+  tidyverse,
+  magrittr,
+  dbplyr
+)
+
+# make db connection
+sf_conn <- DBI::dbConnect(
+  drv = odbc::odbc(),
+  dsn = Sys.getenv("ODBC_DSN_NAME"),
+  uid = Sys.getenv("SNOWFLAKE_USER"), 
+  pwd = Sys.getenv("SNOWFLAKE_PWD")
+)
+
+dat<-tbl(sf_conn,in_schema("PUBLIC","PIM_CASE_CTRL_ASET2")) %>% collect()
+saveRDS(dat,file="./final_aset.rds")
